999a37e1f4947e329e8c80b8b0348ae9e39a91bc
itning <itning@itning.top>
2018-12-14 11:07:12 +0800

    convert entity to kotlin

---
+++ app/src/androidTest/java/top/itning/yunshuclassschedule/ExampleInstrumentedTest.kt
@@ -0,0 +1,27 @@
+package top.itning.yunshuclassschedule
+
+import android.content.Intent
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import top.itning.yunshuclassschedule.service.CourseInfoService
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        appContext.startService(Intent(appContext, CourseInfoService::class.java))
+
+        assertEquals("top.itning.yunshuclassschedule", appContext.packageName)
+    }
+}
