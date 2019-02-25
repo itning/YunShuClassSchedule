48bc54dc26dceec464c86e22046c6d1c5a8933b9
itning <itning@itning.top>
2019-02-25 12:56:09 +0800

    Try Fix #9302 java.io.FileNotFoundException

--- app/src/main/java/top/itning/yunshuclassschedule/ui/activity/ShareActivity.kt
+++ app/src/main/java/top/itning/yunshuclassschedule/ui/activity/ShareActivity.kt
@@ -194,11 +194,11 @@ class ShareActivity : BaseActivity() {
             return
         }
         Log.d(TAG, "File Uri: $uri")
-        val openInputStream = contentResolver.openInputStream(uri) ?: kotlin.run {
-            Toast.makeText(this, "解析失败", Toast.LENGTH_LONG).show()
-            return
-        }
         try {
+            val openInputStream = contentResolver.openInputStream(uri) ?: kotlin.run {
+                Toast.makeText(this, "解析失败", Toast.LENGTH_LONG).show()
+                return
+            }
             val inputAsString = openInputStream.bufferedReader().use { it.readText() }
             val dataEntity = Gson().fromJson(inputAsString, DataEntity::class.java)
             val classScheduleList = dataEntity.classScheduleList
